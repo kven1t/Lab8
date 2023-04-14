215c5ebce265dbb7c175c70c8f14c7cb49745ab4
hp <hp@HP>
2023-04-14 10:46:15 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Lab8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblArr = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.labelSortik = new System.Windows.Forms.Label();
+            this.btnNewArr = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtMax);
+            this.groupBox1.Controls.Add(this.txtMin);
+            this.groupBox1.Controls.Add(this.txtN);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 152);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "параметры массива";
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(184, 103);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(139, 22);
+            this.txtMax.TabIndex = 9;
+            this.txtMax.TextChanged += new System.EventHandler(this.txtMax_TextChanged_1);
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(184, 67);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(139, 22);
+            this.txtMin.TabIndex = 8;
+            this.txtMin.TextChanged += new System.EventHandler(this.txtMin_TextChanged_1);
+            // 
+            // txtN
+            // 
+            this.txtN.Enabled = false;
+            this.txtN.Location = new System.Drawing.Point(207, 27);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(139, 22);
+            this.txtN.TabIndex = 7;
+            this.txtN.TextChanged += new System.EventHandler(this.txtN_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label3.Location = new System.Drawing.Point(6, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "верхняя граница чисел";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label2.Location = new System.Drawing.Point(6, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "нижняя граница чисел";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "количество чисел в массиве";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblArr);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.groupBox2.Location = new System.Drawing.Point(12, 170);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(776, 87);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "исходный массив";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // lblArr
+            // 
+            this.lblArr.AutoSize = true;
+            this.lblArr.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.lblArr.Location = new System.Drawing.Point(6, 50);
+            this.lblArr.Name = "lblArr";
+            this.lblArr.Size = new System.Drawing.Size(0, 16);
+            this.lblArr.TabIndex = 10;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lblResult);
+            this.groupBox3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.groupBox3.Location = new System.Drawing.Point(12, 293);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(776, 81);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "преобразованный массив";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.lblResult.Location = new System.Drawing.Point(6, 50);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 16);
+            this.lblResult.TabIndex = 11;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.labelSortik);
+            this.groupBox4.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.groupBox4.Location = new System.Drawing.Point(12, 399);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(776, 79);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "сортировка вставками";
+            // 
+            // labelSortik
+            // 
+            this.labelSortik.AutoSize = true;
+            this.labelSortik.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.labelSortik.Location = new System.Drawing.Point(6, 52);
+            this.labelSortik.Name = "labelSortik";
+            this.labelSortik.Size = new System.Drawing.Size(0, 16);
+            this.labelSortik.TabIndex = 10;
+            // 
+            // btnNewArr
+            // 
+            this.btnNewArr.AutoSize = true;
+            this.btnNewArr.Location = new System.Drawing.Point(42, 504);
+            this.btnNewArr.Name = "btnNewArr";
+            this.btnNewArr.Size = new System.Drawing.Size(108, 26);
+            this.btnNewArr.TabIndex = 10;
+            this.btnNewArr.Text = "новый массив";
+            this.btnNewArr.UseVisualStyleBackColor = true;
+            this.btnNewArr.Click += new System.EventHandler(this.btnNewArr_Click_1);
+            // 
+            // btnSort
+            // 
+            this.btnSort.AutoSize = true;
+            this.btnSort.Enabled = false;
+            this.btnSort.Location = new System.Drawing.Point(241, 504);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(94, 26);
+            this.btnSort.TabIndex = 11;
+            this.btnSort.Text = "сортировка";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click_1);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(659, 504);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(417, 504);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 26);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Сортировка вставками";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 542);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnNewArr);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNewArr;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblArr;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label labelSortik;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab8
+{
+    public partial class Form1 : Form
+    {
+        private int[] Arr = new int[10];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtN_TextChanged(object sender, EventArgs e)
+        {
+            ClearFields();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ClearFields()
+        {
+            lblArr.Text = "";
+            lblResult.Text = "";
+            labelSortik.Text = "";
+            btnSort.Enabled = false;
+            button1.Enabled = false;
+        }
+
+
+        static int[] Insertion(int[] a)
+        {
+            for (int i = 1; i < a.Length; i++)
+            {
+                int k = a[i];
+                int j = i - 1;
+
+                while (j >= 0 && a[j] > k)
+                {
+                    a[j + 1] = a[j];
+                    a[j] = k;
+                    j--;
+                }
+            }
+            return a;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                labelSortik.Text += Arr[i];
+                if (i != Arr.Length - 1) labelSortik.Text += ", ";
+            }
+            button1.Enabled = true;
+        }
+
+        private void labelSortik_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNewArr_Click_1(object sender, EventArgs e)
+        {
+            int n = 10;
+            int a = int.Parse(txtMin.Text);
+            int b = int.Parse(txtMax.Text);
+            Random r = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                Arr[i] = r.Next(a, b);
+                lblArr.Text += Arr[i];
+                if (i != n) lblArr.Text += ", ";
+            }
+            btnSort.Enabled = true;
+            button1.Enabled = true;
+        }
+
+        private int MinNumber(int[] x, int m)
+        {
+            int min = x[m];
+            int MinN = m;
+            for (int i = m; i < x.Length; i++)
+            {
+                if (x[i] < min)
+                {
+                    min = x[i];
+                    MinN = i;
+                }
+            }
+            return MinN;
+        }
+
+        private void btnSort_Click_1(object sender, EventArgs e)
+        {
+            int k, t;
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                k = MinNumber(Arr, i);
+                t = Arr[i];
+                Arr[i] = Arr[k];
+                Arr[k] = t;
+                lblResult.Text += Arr[i];
+                if (i != Arr.Length - 1) { lblResult.Text += ", "; }
+            }
+            btnSort.Enabled = true;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                labelSortik.Text += Arr[i];
+                if (i != Arr.Length - 1) labelSortik.Text += ", ";
+            }
+            button1.Enabled = true;
+        }
+
+        private void txtMax_TextChanged_1(object sender, EventArgs e)
+        {
+            ClearFields();
+        }
+
+        private void txtMin_TextChanged_1(object sender, EventArgs e)
+        {
+            ClearFields();
+        }
+    }
+}
